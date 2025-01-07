6c266b278939f3e907f8d3980a6892a705c0f0ad
Joshue Abance <iamcoderx@gmail.com>
2025-01-07 14:20:04 +0800

    fix: drop post request for scraper and focus on get request with query params

--- app/router.py
+++ app/router.py
@@ -1,51 +1,31 @@
+from typing import Annotated
+
 import validators
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Query, Response
 
 from app import utils
 from app.handlers import ScrapeBody, handle_scrapers
-from app.handlers.primp import PrimpRequests
 from app.markdown.markdownify import Markdownify
-from app.utils import process_error
 
 api_router = APIRouter(prefix="/r")
 
 
-@api_router.post("/{website:path}")
-async def post_scrape_website(website: str, body: ScrapeBody):
+@api_router.get("/{website:path}")
+async def get_scrape_website(website: str, query: Annotated[ScrapeBody, Query()]):
     if not validators.url(website):
         return Response(content="Invalid URL", status_code=400)
 
-    html_response = handle_scrapers(website, body)
+    html_response = handle_scrapers(website, query)
 
     if not isinstance(html_response, str):
         return html_response
 
-    # process html to markdown
     markdown = ""
 
-    if body.markdown_processor == "markdownify":
+    if query.markdown_processor == "markdownify":
         markdown = Markdownify.process(html_response, website).text
 
-    return Response(content=process_md(markdown), media_type="text/plain")
-
-
-@api_router.get("/{website:path}")
-async def get_scrape_website(website: str):
-    if not validators.url(website):
-        return Response(content="Invalid URL", status_code=400)
-
-    res = PrimpRequests.get(website).res
-
-    if res.status_code != 200:
-        return process_error(res.status_code)
-
-    html_response = res.text
-
-    # process html to markdown
-    markdown = Markdownify.process(html_response, website).text
-
-    return Response(content=process_md(markdown), media_type="text/plain")
-
-
-def process_md(md: str):
-    return utils.remove_excessive_newlines(md).strip()
+    return Response(
+        content=utils.remove_excessive_newlines(markdown).strip(),
+        media_type="text/plain",
+    )
