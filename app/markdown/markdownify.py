58aee401da4cd4ad8e91232cfc383ec42ce460ec
Joshue Abance <iamcoderx@gmail.com>
2025-01-18 16:20:26 +0800

    feat: improve markdown response
    * combine <p> text line breaks to one
    * add title and url and content breakdowns at the top of response

--- app/markdown/markdownify.py
+++ app/markdown/markdownify.py
@@ -1,3 +1,4 @@
+from textwrap import fill
 from urllib.parse import urljoin, urlparse
 
 from markdownify import MarkdownConverter, chomp
@@ -11,6 +12,10 @@ class CustomProcessor(MarkdownConverter):
         # Remove <noscript> tags
         if node.name == "noscript":
             return ""
+
+        if node.name == "title":
+            title = node.text.strip()
+            return f"Title: {title}\nURL: {self.base_url}\n\nCONTENT:\n\n\n"
 
         return super().process_tag(node, convert_as_inline, children_only)
 
@@ -68,6 +73,33 @@ class CustomProcessor(MarkdownConverter):
 
         return "![%s](%s%s)" % (alt, src, title_part)
 
+    def convert_p(self, el, text, convert_as_inline):
+        if convert_as_inline:
+            return " " + text.strip() + " "
+
+        # Join text line breaks.
+        text = " ".join(text.split("\n"))
+
+        if self.options["wrap"]:
+            # Preserve newlines (and preceding whitespace) resulting
+            # from <br> tags.  Newlines in the input have already been
+            # replaced by spaces.
+            lines = text.split("\n")
+            new_lines = []
+            for line in lines:
+                line = line.lstrip()
+                line_no_trailing = line.rstrip()
+                trailing = line[len(line_no_trailing) :]
+                line = fill(
+                    line,
+                    width=self.options["wrap_width"],
+                    break_long_words=False,
+                    break_on_hyphens=False,
+                )
+                new_lines.append(line + trailing)
+            text = "\n".join(new_lines)
+        return "\n\n%s\n\n" % text if text else ""
+
 
 class Markdownify:
     def __init__(self, text: str):
@@ -75,7 +107,7 @@ class Markdownify:
 
     @classmethod
     def process(cls, text, base_url: str):
-        processor = CustomProcessor()
+        processor = CustomProcessor(wrap_width=50)
         processor.base_url = base_url
 
         parsed_url = urlparse(base_url)
